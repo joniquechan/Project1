6e1d5b22f9ebdb83e943f815c7acdf4de0fa3445
joniquechan <uniquejoniqueC@gmail.com>
2024-09-13 13:36:49 -0400

    insertion sort + rand in main

--- src/main/java/com/mycompany/project1algo/InsertionSort.java
+++ src/main/java/com/mycompany/project1algo/InsertionSort.java
@@ -1,5 +1,15 @@
 package com.mycompany.project1algo;
 
 public class InsertionSort {
-    
+    public static void sort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            while (key < arr[j] && j >= 0) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+    }
 }
--- src/main/java/com/mycompany/project1algo/Project1Algo.java
+++ src/main/java/com/mycompany/project1algo/Project1Algo.java
@@ -4,6 +4,8 @@
 
 package com.mycompany.project1algo;
 
+import java.util.Random;
+
 /**
  *
  * @author User
@@ -11,6 +13,26 @@ package com.mycompany.project1algo;
 public class Project1Algo {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int size = 10000;
+        int[] array = new int[size];
+        Random rand = new Random();
+
+        // generate random int from 0-100000
+        for (int i = 0; i < size; i++) {
+            array[i] = rand.nextInt(100000);
+        }
+
+        // measure time for quicksort 1 pointer
+        // measure time for quicksort 2 pointer
+        // measure time for quicksort 1 pointer last pivot
+        // measure time for quicksort 1 pointer median
+        // measure time for quicksort 1 pointer median of 3
+        // measure time for insertionsort random data
+        // measure time for insertionsort reverse sorted data
+        // measure time for insertionsort forward sorted data
+        
+        // function for generating graphs?
+        // overall algorithm performance
+        // partition algorithms
     }
 }
